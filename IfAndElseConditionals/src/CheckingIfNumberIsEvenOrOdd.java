263c37699b32601d7abaed5483dc7aefd53912a7
Diego <diegodecstraid@gmail.com>
2025-09-23 18:21:23 -0300

    Exercice with Checking if a number is even or odd completed.

---
+++ IfAndElseConditionals/src/CheckingIfNumberIsEvenOrOdd.java
@@ -0,0 +1,13 @@
+public class CheckingIfNumberIsEvenOrOdd {
+
+    class ParOuImpar {
+        public static void main(String[] args) {
+            int valor = 7;
+            if (valor % 2 == 0) {
+                System.out.println("O número " + valor + " é par.");
+            } else {
+                System.out.println("O número " + valor + " é ímpar.");
+            }
+        }
+    }
+}
