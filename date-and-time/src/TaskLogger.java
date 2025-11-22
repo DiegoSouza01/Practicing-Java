e3b9ca008ccf6f0c12b9bf7f1b99ee8c6d4094a8
Diego <diegodecstraid@gmail.com>
2025-11-22 15:03:54 -0300

    Exercise with Tasklogger Ending completed.

---
+++ date-and-time/src/TaskLogger.java
@@ -0,0 +1,22 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class TaskLogger {
+
+    public static void main(String[] args) {
+        String tarefa = "Enviar relatório semanal";
+
+        LocalDateTime momentoCriacao = LocalDateTime.now();
+
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm:ss.SSSSSSSSS");
+
+        String dataFormatada = momentoCriacao.format(dateFormat);
+        String horaFormatada = momentoCriacao.format(timeFormat);
+
+        System.out.println("Tarefa: " + tarefa);
+        System.out.println("Data atual: " + dataFormatada);
+        System.out.println("Hora atual: " + horaFormatada);
+    }
+}
