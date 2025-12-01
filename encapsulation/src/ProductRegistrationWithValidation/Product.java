1ca95425c4b3a532db3ae40f9ec8178d89ee4803
Diego <diegodecstraid@gmail.com>
2025-12-01 00:45:49 -0300

    The exercise with Product Registration With Validation has been solved.

---
+++ encapsulation/src/ProductRegistrationWithValidation/Product.java
@@ -0,0 +1,43 @@
+package ProductRegistrationWithValidation;
+
+public class Product {
+    private String nome;
+    private double preco;
+
+    public Product(String nome, double preco) {
+        this.nome = nome;
+        setPreco(preco);
+    }
+
+    public void setPreco(double novoPreco) {
+        if (novoPreco < 0) {
+            System.out.println("Preço inválido. O preço não pode ser negativo.");
+            this.preco = 0.00;
+        } else {
+            this.preco = novoPreco;
+        }
+    }
+
+    public void exibirDados() {
+        System.out.println("Produto: " + nome);
+        System.out.println("Preço: " + String.format("%,.2f", preco));
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public static void main(String[] args) {
+        String nomeExemplo = "Mouse";
+        double precoInvalido = -59.90;
+
+        System.out.println("Teste 1: Preço Negativo");
+        // Ao instanciar, a validação é chamada no construtor
+        Product mouse = new Product(nomeExemplo, precoInvalido);
+        mouse.exibirDados();
+
+        System.out.println("\nTeste 2: Preço Válido");
+        mouse.setPreco(125.50);
+        mouse.exibirDados();
+    }
+}
