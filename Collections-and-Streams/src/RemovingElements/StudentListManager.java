5d2cd1e4923a64dfc7e760168e5cfd45c9d2c05c
Diego <diegodecstraid@gmail.com>
2025-12-09 22:01:42 -0300

    Exercise with Student List Manager has been solved.

---
+++ Collections-and-Streams/src/RemovingElements/StudentListManager.java
@@ -0,0 +1,21 @@
+package RemovingElements;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentListManager {
+    public static void main(String[] args) {
+        List<String> estudantes = new ArrayList<>();
+
+        estudantes.add("Joana");
+        estudantes.add("Lucas");
+        estudantes.add("Pedro");
+        estudantes.add("Antônio");
+
+        System.out.println("Lista inicial: " + estudantes);
+
+        estudantes.remove("Pedro");
+
+        System.out.println("Lista após a exclusão: " + estudantes);
+    }
+}
